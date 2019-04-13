f28d4c4b46397405935c9cd7702bbdae3b801046
Pradip <bhusnarpradip@gmail.com>
2019-04-13 12:00:06 +0530

    Login_page

---
+++ app/src/main/java/com/example/minix/loginFragment.java
@@ -0,0 +1,20 @@
+package com.example.minix;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class loginFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public void login(View view){
+
+    }
+
+    public void signup(View view){
+
+    }
+}
