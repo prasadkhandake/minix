7f352984fde49a04d1080b27a6203b2119179167
kailashgkadam11 <kk5340382@gmail.com>
2019-04-13 10:55:13 +0530

    ssdsd

--- app/src/main/java/com/example/minix/PagerAdapter.java
+++ app/src/main/java/com/example/minix/PagerAdapter.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,6 +16,7 @@ public class PagerAdapter extends android.support.v4.view.PagerAdapter {
     private List<Slide> slides;
     private Context context;
     private LayoutInflater layoutInflater;
+    private int custom_position = 0;
 
     public PagerAdapter(List<Slide> slides, Context context){
         this.slides = slides;
@@ -21,11 +25,21 @@ public class PagerAdapter extends android.support.v4.view.PagerAdapter {
     }
     @Override
     public int getCount() {
-        return 0;
+        return Integer.MAX_VALUE;
     }
 
     @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-        return false;
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) { return view==0; }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        Slide slide = slides.get(position);
+        View itemView = layoutInflater.inflate(R.layout.slider_item_layout,container, false);
+        ImageView mImage = itemView.findViewById(R.id.sliderImage);
+        TextView mTitle = itemView.findViewById(R.id.slider_subtitle);
+        m
+        // return super.instantiateItem(container, position);
     }
 }
